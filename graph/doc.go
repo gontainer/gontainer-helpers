766e0786f6463cd3060fb02bfacb86030f5dbae2
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-09-29 16:49:38 +0400

    test: added examples for `graph` package

---
+++ graph/doc.go
@@ -0,0 +1,2 @@
+// Package graph provides detect circular dependencies and find all dependant nodes in graphs.
+package graph
---
+++ graph/examples_test.go
@@ -0,0 +1,32 @@
+package graph_test
+
+import (
+	"fmt"
+
+	"github.com/gontainer/gontainer-helpers/graph"
+)
+
+func ExampleNew_circularDeps() {
+	g := graph.New()
+	g.AddDep("company", "tech-team")
+	g.AddDep("tech-team", "cto")
+	g.AddDep("cto", "company")
+	g.AddDep("cto", "ceo")
+	g.AddDep("ceo", "company")
+
+	fmt.Println(g.CircularDeps())
+
+	// Output:
+	// [[company tech-team cto company] [company tech-team cto ceo company]]
+}
+
+func ExampleNew_deps() {
+	g := graph.New()
+	g.AddDep("company", "tech-team")
+	g.AddDep("tech-team", "cto")
+
+	fmt.Println(g.Deps("company"))
+
+	// Output:
+	// [cto tech-team]
+}
