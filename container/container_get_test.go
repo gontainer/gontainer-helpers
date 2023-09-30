babc3186f399b122bacb56683284ec5aab2026a4
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-09-30 19:05:09 +0400

    test(container): test error group for service calls

--- container/container_get_test.go
+++ container/container_get_test.go
@@ -11,28 +11,30 @@ import (
 )
 
 func Test_container_executeServiceCalls(t *testing.T) {
-	s := container.NewService()
-	s.SetValue(struct{}{})
-	s.AppendCall("SetName", container.NewDependencyProvider(func() (interface{}, error) {
-		return nil, errors.New("could not fetch the name from the config")
-	}))
-	s.AppendCall("SetAge", container.NewDependencyValue(21))
-	s.AppendCall("SetColor", container.NewDependencyValue("red"))
-	s.AppendWither("WithLogger", container.NewDependencyValue(log.Default()))
-	// this call will be ignored, because it's after the error returned by a wither
-	s.AppendCall("SetLanguage", container.NewDependencyValue("en"))
+	t.Run("Errors", func(t *testing.T) {
+		s := container.NewService()
+		s.SetValue(struct{}{})
+		s.AppendCall("SetName", container.NewDependencyProvider(func() (interface{}, error) {
+			return nil, errors.New("could not fetch the name from the config")
+		}))
+		s.AppendCall("SetAge", container.NewDependencyValue(21))
+		s.AppendCall("SetColor", container.NewDependencyValue("red"))
+		s.AppendWither("WithLogger", container.NewDependencyValue(log.Default()))
+		// this call will be ignored, because it's after the error returned by a wither
+		s.AppendCall("SetLanguage", container.NewDependencyValue("en"))
 
-	c := container.NewContainer()
-	c.OverrideService("service", s)
+		c := container.NewContainer()
+		c.OverrideService("service", s)
 
-	expected := []string{
-		"container.get(\"service\"): resolve args \"SetName\": arg #0: could not fetch the name from the config",
-		"container.get(\"service\"): call \"SetAge\": invalid func `*interface {}`.\"SetAge\"",
-		"container.get(\"service\"): call \"SetColor\": invalid func `*interface {}`.\"SetColor\"",
-		"container.get(\"service\"): wither \"WithLogger\": invalid wither `struct {}`.\"WithLogger\"",
-	}
+		expected := []string{
+			"container.get(\"service\"): resolve args \"SetName\": arg #0: could not fetch the name from the config",
+			"container.get(\"service\"): call \"SetAge\": invalid func `*interface {}`.\"SetAge\"",
+			"container.get(\"service\"): call \"SetColor\": invalid func `*interface {}`.\"SetColor\"",
+			"container.get(\"service\"): wither \"WithLogger\": invalid wither `struct {}`.\"WithLogger\"",
+		}
 
-	svc, err := c.Get("service")
-	assert.Nil(t, svc)
-	assertErr.EqualErrorGroup(t, err, expected)
+		svc, err := c.Get("service")
+		assert.Nil(t, svc)
+		assertErr.EqualErrorGroup(t, err, expected)
+	})
 }
