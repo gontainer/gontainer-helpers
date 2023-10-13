b6c182e9b928a8c32cebc8c0d8684f0fcc75e344
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-13 19:24:18 +0400

    feat(copier): added method ForceCopy

--- copier/copy.go
+++ copier/copy.go
@@ -7,13 +7,29 @@ import (
 	internalReflect "github.com/gontainer/gontainer-helpers/internal/reflect"
 )
 
+// ForceCopy works similar to Copy, but it converts the type whenever it is possible.
+//
+//	var (
+//		from int = 5
+//		to   uint
+//	)
+//	err := copier.ForceCopy(from, &to)
+//	fmt.Println(to) // 5
+func ForceCopy(from interface{}, to interface{}) error {
+	return copyTo(from, to, true)
+}
+
 // Copy copies a value of `from` to `to`.
 //
 //	from := 5
 //	b := 0
 //	Copy(from, &to)
 //	fmt.Println(to) // 5
-func Copy(from interface{}, to interface{}) (err error) {
+func Copy(from interface{}, to interface{}) error {
+	return copyTo(from, to, false)
+}
+
+func copyTo(from interface{}, to interface{}, convert bool) (err error) {
 	defer func() {
 		r := recover()
 		if r == nil {
@@ -22,11 +38,20 @@ func Copy(from interface{}, to interface{}) (err error) {
 		err = fmt.Errorf("%s", r)
 	}()
 
-	f := reflect.ValueOf(from)
 	t := reflect.ValueOf(to)
 
 	if t.Kind() != reflect.Ptr {
 		return fmt.Errorf("expected pointer, `%s` given", t.Kind())
+	}
+
+	var f reflect.Value
+	if convert {
+		f, err = internalReflect.Convert(from, t.Elem().Type())
+		if err != nil {
+			return err
+		}
+	} else {
+		f = reflect.ValueOf(from)
 	}
 
 	if from == nil && internalReflect.IsNilable(t.Elem().Kind()) {
