3ab63df1beff91a60b9882139ef56899df7a2532
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-17 18:28:21 +0400

    benchmark(container): added a benchmark for `scopeNonShared`

--- container/benchmark_test.go
+++ container/benchmark_test.go
@@ -20,7 +20,7 @@ func BenchmarkNewContainer_container_scopeDefault(b *testing.B) {
 	e.SetField("Name", container.NewDependencyValue("Mary"))
 	e.ScopeDefault()
 	c.OverrideService("employee", e)
-	_, _ = c.Get("employee")
+	_, _ = c.Get("employee") // warm up
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
@@ -37,7 +37,7 @@ func BenchmarkNewContainer_container_scopeShared(b *testing.B) {
 	e.SetField("Name", container.NewDependencyValue("Mary"))
 	e.ScopeShared()
 	c.OverrideService("employee", e)
-	_, _ = c.Get("employee")
+	_, _ = c.Get("employee") // warm up
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
@@ -54,7 +54,7 @@ func BenchmarkNewContainer_container_scopeContextual(b *testing.B) {
 	e.SetField("Name", container.NewDependencyValue("Mary"))
 	e.ScopeContextual()
 	c.OverrideService("employee", e)
-	_, _ = c.Get("employee")
+	_, _ = c.Get("employee") // warm up
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
@@ -72,11 +72,28 @@ func BenchmarkNewContainer_container_scopeContextual_in_same_context(b *testing.
 	e.ScopeContextual()
 	c.OverrideService("employee", e)
 	ctx := container.ContextWithContainer(context.Background(), c)
-	_, _ = c.GetInContext(ctx, "employee")
+	_, _ = c.GetInContext(ctx, "employee") // warm up
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		_, _ = c.GetInContext(ctx, "employee")
+	}
+}
+
+func BenchmarkNewContainer_container_scopeNonShared(b *testing.B) {
+	c := container.NewContainer()
+	e := container.NewService()
+	e.SetConstructor(func() interface{} {
+		return Employee{}
+	})
+	e.SetField("Name", container.NewDependencyValue("Mary"))
+	e.ScopeNonShared()
+	c.OverrideService("employee", e)
+	_, _ = c.Get("employee") // warm up
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = c.Get("employee")
 	}
 }
 
