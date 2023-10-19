d7177e8f52dbee48f9dc2f0afc16ac2a407ddb05
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-19 21:14:26 +0400

    feat(caller): added `Assert*` funcs

--- caller/assert.go
+++ caller/assert.go
@@ -4,25 +4,31 @@ import (
 	"github.com/gontainer/gontainer-helpers/internal/caller"
 )
 
-func AssertFunc(fn interface{}) {
+// TODO: do we need it? do we need a second argument to check the number of args?
+
+// AssertFunc panics if the given input is not a func.
+func AssertFunc(fn any) {
 	if _, err := caller.Func(fn); err != nil {
 		panic(err)
 	}
 }
 
-func AssertProvider(fn interface{}) {
+// AssertProvider panics if the given input is not a provider.
+func AssertProvider(fn any) {
 	if _, err := caller.FuncProvider(fn); err != nil {
 		panic(err)
 	}
 }
 
-func AssertMethod(object interface{}, method string) {
+// AssertMethod panics if the given input is not a method.
+func AssertMethod(object any, method string) {
 	if _, err := caller.Method(object, method); err != nil {
 		panic(err)
 	}
 }
 
-func AssertWither(object interface{}, method string) {
+// AssertWither panics if the given input is not a wither.
+func AssertWither(object any, method string) {
 	if _, err := caller.Wither(object, method); err != nil {
 		panic(err)
 	}
