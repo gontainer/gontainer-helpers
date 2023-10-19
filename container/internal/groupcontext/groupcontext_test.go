c86606e973574119bcd7ac1eac413832de9105d8
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-19 22:31:24 +0400

    test(container/internal/groupcontext): improve tests

--- container/internal/groupcontext/groupcontext_test.go
+++ container/internal/groupcontext/groupcontext_test.go
@@ -15,18 +15,18 @@ func TestNew(t *testing.T) {
 	t.Run("Context already cancelled", func(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 		cancel()
-		done := new(int64)
+		counter := new(int64)
 
 		g := groupcontext.New()
 		g.Add(ctx)
 
 		go func() {
 			g.Wait()
-			atomic.AddInt64(done, 1)
+			atomic.AddInt64(counter, 1)
 		}()
 		time.Sleep(time.Millisecond * 100)
 
-		assert.Equal(t, int64(1), atomic.LoadInt64(done))
+		assert.Equal(t, int64(1), atomic.LoadInt64(counter))
 	})
 
 	t.Run("Context never cancelled", func(t *testing.T) {
@@ -37,18 +37,18 @@ func TestNew(t *testing.T) {
 		for i, ctx := range ctxs {
 			ctx := ctx
 			t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
-				done := new(int64)
+				counter := new(int64)
 
 				g := groupcontext.New()
 				g.Add(ctx)
 
 				go func() {
 					g.Wait()
-					atomic.AddInt64(done, 1)
+					atomic.AddInt64(counter, 1)
 				}()
 				time.Sleep(time.Millisecond * 100)
 
-				assert.Equal(t, int64(0), atomic.LoadInt64(done))
+				assert.Equal(t, int64(0), atomic.LoadInt64(counter))
 			})
 		}
 	})
@@ -58,22 +58,27 @@ func TestNew(t *testing.T) {
 		ctx2, cancel2 := context.WithCancel(context.Background())
 		childCtx2 := context.WithValue(ctx2, "my-key", "my-value") //nolint:staticcheck
 
+		counter := new(int64)
+
 		g := groupcontext.New()
 		g.Add(ctx1)
 		g.Add(childCtx2)
 
 		go func() {
 			time.Sleep(time.Millisecond * 100)
+			atomic.AddInt64(counter, 1)
 			cancel1()
 		}()
 
 		go func() {
 			time.Sleep(time.Millisecond * 200)
+			atomic.AddInt64(counter, 1)
 			cancel2() // it cancels the child context as well
 		}()
 
 		s := time.Now()
 		g.Wait()
 		assert.GreaterOrEqual(t, time.Since(s), time.Millisecond*200)
+		assert.Equal(t, int64(2), atomic.LoadInt64(counter))
 	})
 }
