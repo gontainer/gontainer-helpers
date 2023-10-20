26ace941d7238519a8e6518c2678ffc05e682abf
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-21 01:02:17 +0400

    chore: deprecation

--- copier/copy.go
+++ copier/copy.go
@@ -7,24 +7,30 @@ import (
 	intReflect "github.com/gontainer/gontainer-helpers/internal/reflect"
 )
 
-// ConvertAndCopy works similar to Copy, but it converts the type whenever it is possible.
-//
-//	var (
-//		from int = 5
-//		to   uint
-//	)
-//	err := copier.ConvertAndCopy(from, &to)
-//	fmt.Println(to) // 5
+/*
+ConvertAndCopy works similar to Copy, but it converts the type whenever it is possible.
+
+	var (
+		from int = 5
+		to   uint
+	)
+	err := copier.ConvertAndCopy(from, &to)
+	fmt.Println(to) // 5
+
+Deprecated: add param convert to func Copy
+*/
 func ConvertAndCopy(from any, to any) error {
 	return copyTo(from, to, true)
 }
 
-// Copy copies a value of `from` to `to`.
-//
-//	from := 5
-//	b := 0
-//	Copy(from, &to)
-//	fmt.Println(to) // 5
+/*
+Copy copies a value of `from` to `to`.
+
+	from := 5
+	b := 0
+	Copy(from, &to)
+	fmt.Println(to) // 5
+*/
 func Copy(from any, to any) error {
 	return copyTo(from, to, false)
 }
