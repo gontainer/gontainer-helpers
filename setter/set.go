5254c3806c9dd4665a0a7a82a6cf3401cc982574
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-21 10:37:33 +0400

    doc: setter

--- setter/set.go
+++ setter/set.go
@@ -1,7 +1,16 @@
 package setter
 
-// Set assigns the value `val` to the field `field` on the struct `strct`.
-// Unexported fields are supported.
+/*
+Set assigns the value `val` to the field `field` on the struct `strct`.
+Unexported fields are supported.
+
+	type Person struct {
+		Name string
+	}
+	p := Person{}
+	_ = setter.Set(&p, "Name", "Jane", false)
+	fmt.Println(p) // {Jane}
+*/
 func Set(strct any, field string, val any, convert bool) error {
 	return set(strct, field, val, convert)
 }
