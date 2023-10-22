27233130b90a6113a02749548d727ad2fa56b134
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-22 23:50:37 +0400

    docs(setter): go doc

--- setter/doc.go
+++ setter/doc.go
@@ -1,2 +1,14 @@
-// Package setter allows for manipulation of a value of an exported field of any struct.
+/*
+Package setter allows for manipulation of a value of an exported field of any struct.
+
+	person := struct {
+		name string
+	}{}
+	err := setter.Set(&person, "name", "Mary", false)
+	fmt.Println(person.name)
+	fmt.Println(err)
+	// Output:
+	// Mary
+	// <nil>
+*/
 package setter
