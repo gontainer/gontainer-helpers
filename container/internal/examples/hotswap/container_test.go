c68c5ecc8b929e932879cdacaf25531d3ec00af5
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-22 10:54:24 +0400

    test(container): `HotSwap`

--- container/internal/examples/hotswap/container_test.go
+++ container/internal/examples/hotswap/container_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gontainer/gontainer-helpers/v2/container"
 	"github.com/gontainer/gontainer-helpers/v2/container/internal/examples/hotswap"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,44 +22,51 @@ func TestContainer_Server(t *testing.T) {
 	s := httptest.NewServer(c.ServeMux())
 	defer s.Close()
 
-	wg := sync.WaitGroup{}
-
 	client := s.Client()
 
-	for i := 0; i < max; i++ {
-		i := i
-		wg.Add(2)
-		go func() {
-			defer wg.Done()
+	runGoroutines := func(hotSwap bool) (consistent bool) {
+		consistent = true
+		wg := sync.WaitGroup{}
+		for i := 0; i < max; i++ {
+			i := i
+			wg.Add(2)
+			go func() {
+				defer wg.Done()
 
-			r, err := client.Get(s.URL + "/")
-			require.NoError(t, err)
+				r, err := client.Get(s.URL)
+				require.NoError(t, err)
 
-			buff, err := ioutil.ReadAll(r.Body)
-			require.NoError(t, err)
+				buff, err := ioutil.ReadAll(r.Body)
+				require.NoError(t, err)
 
-			vals := strings.SplitN(string(buff), "=", 2)
+				vals := strings.SplitN(string(buff), "=", 2)
 
-			require.Equal(t, vals[0], vals[1])
-		}()
-		go func() {
-			defer wg.Done()
+				consistent = consistent && vals[0] == vals[1]
+			}()
+			go func() {
+				defer wg.Done()
 
-			// the commented code does not guarantee atomicity
-			// we have to use HotSwap
-			// c.OverrideParam("a", container.NewDependencyValue(i))
-			// time.Sleep(time.Millisecond * 2)
-			// c.OverrideParam("b", container.NewDependencyValue(i))
+				if hotSwap { // HotSwap guarantees atomicity
+					c.HotSwap(func(c container.MutableContainer) {
+						c.OverrideParam("a", container.NewDependencyValue(i))
+						// sleep to simulate an edge case
+						time.Sleep(time.Millisecond * 2)
+						c.OverrideParam("b", container.NewDependencyValue(i))
+					})
+					return
+				}
 
-			// HotSwap guarantees atomicity
-			c.HotSwap(func(c container.MutableContainer) {
+				// Changing the following params is not an atomic operation.
+				// It is possible that another goroutines read a new value of "a", and a new value of "b".
 				c.OverrideParam("a", container.NewDependencyValue(i))
-				// sleep to simulate an edge case
 				time.Sleep(time.Millisecond * 2)
 				c.OverrideParam("b", container.NewDependencyValue(i))
-			})
-		}()
+			}()
+		}
+		wg.Wait()
+		return consistent
 	}
 
-	wg.Wait()
+	assert.True(t, runGoroutines(true))
+	assert.False(t, runGoroutines(false))
 }
