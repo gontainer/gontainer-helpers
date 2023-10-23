81b93fe72b6c7f35918a0f4dd17cfbb2872b255d
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-23 20:02:10 +0400

    docs: grouperror

--- grouperror/doc.go
+++ grouperror/doc.go
@@ -1,5 +1,7 @@
-// Package grouperror provides a toolset to join and split errors.
-//
-//	err := errors.Prefix("my group: ", fmt.Errorf("error1), nil, fmt.Errorf("error2"))
-//	errs := errors.Collection(err) // []errors{error("my group: error1"), error("my group: error2")}
+/*
+Package grouperror provides a toolset to join and split errors.
+
+	err := grouperror.Prefix("my group: ", fmt.Errorf("error1), nil, fmt.Errorf("error2"))
+	errs := grouperror.Collection(err) // []errors{error("my group: error1"), error("my group: error2")}
+*/
 package grouperror
