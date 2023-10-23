7eed91e27760b9c79f2550e7f362838289dd246b
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-23 23:19:47 +0400

    docs: removed todo comment

--- container/http.go
+++ container/http.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 )
 
-// TODO is it a good idea to add it to this package?
-
 // HTTPHandlerWithContainer creates a new HTTP handler that automatically binds contexts with the container.
 func HTTPHandlerWithContainer(handler http.Handler, container Self) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
