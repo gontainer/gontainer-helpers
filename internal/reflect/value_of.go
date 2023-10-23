48c57a8b2bf51245bf760eab73f69c32b4e502ae
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-23 19:54:19 +0400

    docs: internal

--- internal/reflect/value_of.go
+++ internal/reflect/value_of.go
@@ -11,6 +11,7 @@ ValueOf is an extended version of [reflect.ValueOf].
 Built-in [reflect.ValueOf](nil) returns the zero [reflect.Value].
 ValueOf for the `i == nil` and a nil-able [reflect.Kind] of `to` returns a zero value from `to`.
 
+If the third argument equals true, it converts the type whenever it is possible,
 If `result.Type()` is not assignable to `to` it returns an error.
 */
 func ValueOf(i any, to reflect.Type, convert_ bool) (result reflect.Value, err error) {
