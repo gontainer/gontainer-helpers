a55e93c79f5bd10b5828df42d379e53611701a93
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-23 20:04:56 +0400

    docs: container

--- container/container_decorators.go
+++ container/container_decorators.go
@@ -1,6 +1,8 @@
 package container
 
 // DecoratorPayload is the very first argument passed to every decorator always.
+//
+// See [*Container.AddDecorator].
 type DecoratorPayload struct {
 	Tag       string
 	ServiceID string
@@ -9,6 +11,7 @@ type DecoratorPayload struct {
 
 // AddDecorator adds decorator for the given tag.
 // Decorator is a special function that can decorate all services tagged by the given tag.
+//
 // See [DecoratorPayload].
 func (c *Container) AddDecorator(tag string, decorator any, deps ...Dependency) {
 	c.globalLocker.Lock()
