b89d316bad459b2490c397200d4fa29e4de801b8
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-31 18:16:01 +0400

    test(`container.ContextWithContainer`): added test for nested contexts

--- container/container_context_test.go
+++ container/container_context_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gontainer/gontainer-helpers/v2/container"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type myWrappedContainer struct {
@@ -19,6 +20,45 @@ func newMyWrappedContainer() *myWrappedContainer {
 }
 
 func TestContextWithContainer(t *testing.T) {
+	t.Run("Nested context", func(t *testing.T) {
+		c := container.New()
+		s := container.NewService()
+		s.SetConstructor(func() any {
+			return new(int64)
+		})
+		s.SetScopeContextual()
+		c.OverrideService("ptr", s)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		ctx = container.ContextWithContainer(ctx, c)
+
+		ctxNested := container.ContextWithContainer(ctx, c)
+
+		// the following line creates a context with some dummy values,
+		// because we want ctxNested != ctxNested2
+		type dummyKey string
+		ctxNested2 := context.WithValue(ctxNested, dummyKey("key"), "dummy value")
+		ctxNested2 = container.ContextWithContainer(ctxNested2, c)
+
+		ctxAnother, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		ctxAnother = container.ContextWithContainer(ctxAnother, c)
+
+		ptr1, err1 := c.GetInContext(ctx, "ptr")
+		ptr2, err2 := c.GetInContext(ctxNested, "ptr")
+		ptr3, err3 := c.GetInContext(ctxNested2, "ptr")
+		ptr4, err4 := c.GetInContext(ctxAnother, "ptr")
+
+		require.NoError(t, err1)
+		require.NoError(t, err2)
+		require.NoError(t, err3)
+		require.NoError(t, err4)
+
+		assert.Same(t, ptr1, ptr2, "Nested context should inherit contextBag from the parent")
+		assert.Same(t, ptr1, ptr3, "Nested context should inherit contextBag from the parent")
+		assert.NotSame(t, ptr1, ptr4)
+	})
 	t.Run("Wrapped *Container", func(t *testing.T) {
 		// Make sure that approach (embedding struct implementing an interface with unexported methods)
 		// works in all GO's versions
