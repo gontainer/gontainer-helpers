028b65ae77a37db910313561c665eb9e6906264c
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-31 23:02:29 +0400

    docs: container

--- container/examples_test.go
+++ container/examples_test.go
@@ -46,6 +46,61 @@ type People struct {
 	People []Person
 }
 
+type Superhero struct {
+	Name string
+}
+
+func NewSuperhero(name string) Superhero {
+	return Superhero{Name: name}
+}
+
+type Team struct {
+	Superheroes []Superhero
+}
+
+func buildContainer() *container.Container {
+	// describe Iron Man
+	ironMan := container.NewService()
+	ironMan.SetValue(Superhero{})
+	ironMan.SetField("Name", container.NewDependencyValue("Iron Man"))
+	ironMan.Tag("avengers", 0)
+
+	// describe Thor
+	thor := container.NewService()
+	thor.SetValue(Superhero{
+		Name: "Thor",
+	})
+	thor.Tag("avengers", 1) // Thor has a higher priority
+
+	// describe Hulk
+	hulk := container.NewService()
+	hulk.SetConstructor(
+		NewSuperhero,
+		container.NewDependencyValue("Hulk"),
+	)
+	hulk.Tag("avengers", 0)
+
+	// describe Avengers
+	avengers := container.NewService()
+	avengers.SetValue(Team{})
+	avengers.SetField("Superheroes", container.NewDependencyTag("avengers"))
+
+	c := container.New()
+	c.OverrideService("ironMan", ironMan)
+	c.OverrideService("thor", thor)
+	c.OverrideService("hulk", hulk)
+	c.OverrideService("avengers", avengers)
+
+	return c
+}
+
+func Example() {
+	c := buildContainer()
+	avengers, _ := c.Get("avengers")
+	fmt.Printf("%+v\n", avengers)
+	// Output: {Superheroes:[{Name:Thor} {Name:Hulk} {Name:Iron Man}]}
+}
+
 func ExampleContainer_GetInContext_wrongContext() {
 	c := container.New()
 
