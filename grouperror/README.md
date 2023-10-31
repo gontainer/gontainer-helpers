47689ba38924f3b8e3d094da18f8afe6356568a9
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-10-31 20:53:05 +0400

    docs: grouperror

--- grouperror/README.md
+++ grouperror/README.md
@@ -3,7 +3,7 @@
 This package provides a toolset to join and split errors.
 
 ```go
-err := grouperror.Prefix("my group: ", fmt.Errorf("error1), nil, fmt.Errorf("error2"))
+err := grouperror.Prefix("my group: ", fmt.Errorf("error1"), nil, fmt.Errorf("error2"))
 errs := grouperror.Collection(err) // []errors{error("my group: error1"), error("my group: error2")}
 ```
 
--- grouperror/doc.go
+++ grouperror/doc.go
@@ -1,7 +1,7 @@
 /*
 Package grouperror provides a toolset to join and split errors.
 
-	err := grouperror.Prefix("my group: ", fmt.Errorf("error1), nil, fmt.Errorf("error2"))
+	err := grouperror.Prefix("my group: ", fmt.Errorf("error1"), nil, fmt.Errorf("error2"))
 	errs := grouperror.Collection(err) // []errors{error("my group: error1"), error("my group: error2")}
 */
 package grouperror
