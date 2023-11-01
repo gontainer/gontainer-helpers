ea6d7a9a5c60f3899150f9a337791bd884cbeee4
bkrukowski <krukowski.bartlomiej@gmail.com>
2023-11-01 23:47:34 +0400

    docs: errors

--- grouperror/errors.go
+++ grouperror/errors.go
@@ -32,7 +32,7 @@ func Join(errs ...error) error {
 	return Prefix("", errs...)
 }
 
-// Prefix joins error the same way as [Join], and adds a prefix to the group.
+// Prefix joins errors the same way as [Join], and adds a prefix to the group.
 func Prefix(prefix string, errs ...error) error {
 	n := 0
 	for _, err := range errs {
